ca2c1b336d6f253b18dada99387a285a95133eee
Anietri <osvaldo.nieto@alumnos.udg.mx>
2023-06-02 15:56:59 -0600

    Aquí está corregida la tercera práctica

--- 2.1_Tercera_practica.py
+++ 2.1_Tercera_practica.py
@@ -1,6 +1,7 @@
 cantidaddeman = int(input("Cuántas manzanas vas a comprar? "))
 preciodeman = int(input("Cuál es el precio de las manzanas? "))
+total = (cantidaddeman * preciodeman)
 print("El precio de las manzanas que te llevas es: ")
-print(cantidaddeman * preciodeman)
+print(total)
 #Concatenación
-print(f"Como te estás llevando: {cantidaddeman} y cuestan : {preciodeman} vas a pagar {cantidaddeman * preciodeman}")
+print(f"Como te estás llevando: {cantidaddeman} y cuestan : {preciodeman} vas a pagar {total}")
