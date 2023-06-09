d6febd012953185f4134f6736a6cc35854fc8150
Anietri <osvaldo.nieto@alumnos.udg.mx>
2023-06-09 14:49:27 -0600

    Agragué 2 programas que corren

---
+++ 10_While_Manzana.py
@@ -0,0 +1,16 @@
+cantidaddeman = int(input("Cuántas manzanas vas a comprar? "))
+preciodeman = int(0)
+total = int(0)
+descuento = int(0)
+while cantidaddeman != 0:
+    preciodeman = int(input("Cuál es el precio de las manzanas? "))
+    nombre = input("¿Cuál es tu nombre?    ")
+    total = (cantidaddeman * preciodeman)
+    descuento:float = 0
+    if(cantidaddeman == 18 or nombre == "andres"):
+        total = total * .8
+        descuento = (cantidaddeman * preciodeman)*.2
+    elif(cantidaddeman > 10): 
+        total = total * .9
+        descuento = (cantidaddeman * preciodeman)*.1
+    print(f"Como te estás llevando: {cantidaddeman} y cuestan : {preciodeman} vas a pagar {total} y te ahorraste {descuento}")
---
+++ 9_while.py
@@ -0,0 +1,4 @@
+multiplo = 1
+while multiplo <= 100:
+    print(f"El resultado de 5 por  {multiplo} es igual a: {5*multiplo} " )
+    multiplo+=1
